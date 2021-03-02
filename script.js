7bbafe46354ef7ff415823150cd88a4cc13f4869
Renato Magrini <renatomagrini@gmail.com>
2021-03-02 09:00:47 +0100

    updating

--- script.js
+++ script.js
@@ -10,18 +10,14 @@ const load = () => {
   if (now < 6) {
     msg.innerHTML += `<p>Good Night...</p>`;
     pic.src = "./img/night.png";
-    document.body.style.background = "#798EA2";
   } else if (now < 12) {
     msg.innerHTML += `<p>Good Morning!</p>`;
     pic.src = "./img/morning.png";
-    document.body.style.background = "#ffee93";
   } else if (now < 18) {
     msg.innerHTML += `<p>Good Afternoon!</p>`;
     pic.src = "./img/afternoon.png";
-    document.body.style.background = "#a1cae2";
   } else {
     msg.innerHTML += `<p>Good Evening!</p>`;
     pic.src = "./img/night.png";
-    document.body.style.background = "#798EA2";
   }
 };
