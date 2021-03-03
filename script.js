af910148d1ccf753870d75a4cf8ebb1b9bf9e2cb
Renato Magrini <renatomagrini@gmail.com>
2021-03-03 05:43:36 +0100

    minutes

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const load = () => {
-  const now = new Date().getHours();
+  const hour = new Date().getHours();
+  const min = new Date().getMinutes();
   // const now = 10;
 
   const msg = document.querySelector("#msg");
   const pic = document.querySelector("#pic");
 
-  msg.innerHTML = `Current Local Time: ${now}h.`;
+  msg.innerHTML = `Current Local Time: ${hour}h${min}min.`;
 
-  if (now < 6) {
+  if (hour < 6) {
     msg.innerHTML += `<p>Good Night...</p>`;
     pic.src = "./img/night.png";
-  } else if (now < 12) {
+  } else if (hour < 12) {
     msg.innerHTML += `<p>Good Morning!</p>`;
     pic.src = "./img/morning.png";
-  } else if (now < 18) {
+  } else if (hour < 18) {
     msg.innerHTML += `<p>Good Afternoon!</p>`;
     pic.src = "./img/afternoon.png";
   } else {
